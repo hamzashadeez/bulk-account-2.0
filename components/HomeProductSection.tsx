06f6141f76584b762c73cc2c48d8d0d2af0c0cd8
Hamza Shadeez <hahmad1178@gmail.com>
2025-06-06 23:17:53 +0100

    yes

--- components/HomeProductSection.tsx
+++ components/HomeProductSection.tsx
@@ -29,7 +29,7 @@ function HomeProductSection() {
   return (
     <div className='py-12 px-4 md:px-8 lg:px-24 bg-gray-100'>
         <h3 className='text-center mb-10 font-semibold text-lg'>Our Products</h3>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product: any) => (
           <div key={product._id} className="bg-white rounded-lg shadow-md p-4 w-full">
             <img src="/logo.jpg" alt="" />
@@ -37,9 +37,9 @@ function HomeProductSection() {
             <p className="text-gray-600">Followers: {product.followers.toLocaleString()}</p>
             <p className="text-gray-600">Following: {product.following.toLocaleString()}</p>
             <p className="text-green-700 font-bold">Price: ₦{product.price.toLocaleString()}</p>
-            <p className='text-xs py-2'>Status : {product.available ? "Available" : "Not Available"}</p>
+            <p className='text-xs py-2 mb-5'>Status : {product.available ? "Available" : "Not Available"}</p>
             <Link href={`/p/${product._id}`} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-              <p className='text-sm'>View</p>
+              View Details
             </Link>
         </div>
         ))}
