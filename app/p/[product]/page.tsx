ad5df7a9eecf321fbd964e0675ec88cc3e71edda
Hamza Shadeez <hahmad1178@gmail.com>
2025-06-06 23:13:47 +0100

    still update

---
+++ app/p/[product]/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import apiClient from "@/lib/apiClient";
+import { useParams } from "next/navigation";
+import React, { useEffect } from "react";
+// import { EditProduct } from "./EditProduct";
+
+function SingleProduct() {
+  const { product } = useParams();
+  const [productData, setProductData]: any = React.useState(null);
+  const getProduct = async () => {
+    const res: any = await apiClient.get(`/product/${product}`);
+    if (res.ok) {
+      console.log(res.data);
+      setProductData(res.data);
+    }
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+  console.log(product);
+  return (
+    <div className="w-5/6 mx-auto my-10">
+      <h2 className="text-lg mb-5 uppercase font-medium text-green-700">
+        Account Details
+      </h2>
+      <h1 className="">
+        Platform:{" "}
+        <span className="font-semibold text-green-700 text-lg uppercase">
+          {productData?.platform}
+        </span>
+      </h1>
+      <p>
+        Followers:{" "}
+        <span className="font-semibold text-lg text-green-700">
+          {productData?.followers}
+        </span>
+      </p>
+      <p>
+        Following:{" "}
+        <span className="font-semibold text-lg text-green-700">
+          {productData?.following}
+        </span>
+      </p>
+      <p>
+        Price:{" "}
+        <span className="font-semibold text-lg text-green-700">
+          ₦{productData?.price.toLocaleString()}
+        </span>
+      </p>
+
+      <div className="mt-10 flex gap-6 items-center justify-center">
+        {/* <EditProduct data={productData} /> */}
+        <Button className="cursor-pointer" variant={"destructive"}>
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SingleProduct;
--- components/HomeProductSection.tsx
+++ components/HomeProductSection.tsx
@@ -1,5 +1,6 @@
 "use client"
 import apiClient from '@/lib/apiClient';
+import Link from 'next/link';
 import React, { useEffect } from 'react'
 
 
@@ -26,18 +27,20 @@ function HomeProductSection() {
 
 
   return (
-    <div className='py-12 px-4 md:px-8 lg:px-24'>
-        <h3>Our Products</h3>
+    <div className='py-12 px-4 md:px-8 lg:px-24 bg-gray-100'>
+        <h3 className='text-center mb-10 font-semibold text-lg'>Our Products</h3>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {products.map((product: any) => (
-          <div key={product._id} className="bg-white rounded-lg shadow-md p-4">
-            <h4 className="text-lg font-semibold mb-2">{product.platform}</h4>
-            <p className="text-gray-600">Followers: {product.followers}</p>
-            <p className="text-gray-600">Following: {product.following}</p>
-            <p className="text-green-700 font-bold">₦{product.price.toLocaleString()}</p>
-            <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-              Buy Now
-            </button>
+          <div key={product._id} className="bg-white rounded-lg shadow-md p-4 w-full">
+            <img src="/logo.jpg" alt="" />
+            <h4 className="text-lg font-semibold mb-2 capitalize">Platform: {product.platform}</h4>
+            <p className="text-gray-600">Followers: {product.followers.toLocaleString()}</p>
+            <p className="text-gray-600">Following: {product.following.toLocaleString()}</p>
+            <p className="text-green-700 font-bold">Price: ₦{product.price.toLocaleString()}</p>
+            <p className='text-xs py-2'>Status : {product.available ? "Available" : "Not Available"}</p>
+            <Link href={`/p/${product._id}`} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+              <p className='text-sm'>View</p>
+            </Link>
         </div>
         ))}
         </div>
