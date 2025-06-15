c487fcb2700bf315c0c758ff4e51f3aa118df349
Hamza Shadeez <hahmad1178@gmail.com>
2025-06-15 23:51:10 +0100

    added skeleton

--- components/HomeProductSection.tsx
+++ components/HomeProductSection.tsx
@@ -1,51 +1,89 @@
-"use client"
-import apiClient from '@/lib/apiClient';
-import Link from 'next/link';
-import React, { useEffect } from 'react'
-
+"use client";
+import apiClient from "@/lib/apiClient";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 function HomeProductSection() {
   const [products, setProducts]: any = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
-     const getProducts = async () => {
+  const getProducts = async () => {
     let aval: any = [];
+    setLoading(true);
 
     const res: any = await apiClient.get("/product");
     if (res.ok) {
       setProducts(res.data);
       console.log(res.data);
-      
+      setLoading(false);
     } else {
       console.log(res);
+      setLoading(false);
       throw new Error("Failed to fetch products");
     }
   };
 
-  useEffect(() => { 
+  useEffect(() => {
     getProducts();
   }, []);
 
-
   return (
-    <div className='py-12 px-4 md:px-8 lg:px-24 bg-gray-100'>
-        <h3 className='text-center mb-10 font-semibold text-lg'>Our Products</h3>
+    <div className="py-12 px-4 md:px-8 lg:px-24 bg-gray-100">
+      <h3 className="text-center mb-10 font-semibold text-lg">Our Products</h3>
+      {loading ? (
+       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+         <div className="flex flex-col gap-4">
+          <Skeleton className="h-20 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+        </div>
+         <div className="flex flex-col gap-4">
+          <Skeleton className="h-20 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+        </div>
+         <div className="flex flex-col gap-4">
+          <Skeleton className="h-20 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+          <Skeleton className="h-10 w-full bg-gray-300" />
+        </div>
+       </div>
+      ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {products.map((product: any) => (
-          <div key={product._id} className="bg-white rounded-lg shadow-md p-4 w-full">
-            <img src="/logo.jpg" alt="" />
-            <h4 className="text-lg font-semibold mb-2 capitalize">Platform: {product.platform}</h4>
-            <p className="text-gray-600">Followers: {product.followers.toLocaleString()}</p>
-            <p className="text-gray-600">Following: {product.following.toLocaleString()}</p>
-            <p className="text-green-700 font-bold">Price: ₦{product.price.toLocaleString()}</p>
-            <p className='text-xs py-2 mb-5'>Status : {product.available ? "Available" : "Not Available"}</p>
-            <Link href={`/p/${product._id}`} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
-              View Details
-            </Link>
+          {products.map((product: any) => (
+            <div
+              key={product._id}
+              className="bg-white rounded-lg shadow-md p-4 w-full"
+            >
+              <img src="/logo.jpg" alt="" />
+              <h4 className="text-lg font-semibold mb-2 capitalize">
+                Platform: {product.platform}
+              </h4>
+              <p className="text-gray-600">
+                Followers: {product.followers.toLocaleString()}
+              </p>
+              <p className="text-gray-600">
+                Following: {product.following.toLocaleString()}
+              </p>
+              <p className="text-green-700 font-bold">
+                Price: ₦{product.price.toLocaleString()}
+              </p>
+              <p className="text-xs py-2 mb-5">
+                Status : {product.available ? "Available" : "Not Available"}
+              </p>
+              <Link
+                href={`/p/${product._id}`}
+                className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+              >
+                View Details
+              </Link>
+            </div>
+          ))}
         </div>
-        ))}
-        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default HomeProductSection
+export default HomeProductSection;
